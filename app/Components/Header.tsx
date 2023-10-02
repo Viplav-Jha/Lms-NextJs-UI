bfd804a661b13249c4e9f1704e68f540faf89cf1
Viplav jha <viplavjha1993@gmail.com>
2023-10-02 19:56:31 +0530

    Header logics

--- app/Components/Header.tsx
+++ app/Components/Header.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Link from "next/link";
 import { useState, FC } from "react";
 
@@ -10,42 +10,44 @@ type Props = {
 
  const Header: FC<Props> = (props) => {
   const [active, setActive] = useState(false);
-  const [openSidebar, setOpenSidebar] = useState(false);
+  const [openSidebar, setOpenSidebar] = useState(true);
 
   if(typeof window !== "undefined"){
     window.addEventListener("scroll",()=>{
       if(window.scrollY > 85){
         setActive(true);
+        
       }else {
         setActive(false)
       }
     })
-  }
+  };
+ 
 
   
   return (
     <div className="w-full relative">
-      <div
-        className={`${
-          active
-            ? "dark:bg-opacity-50 dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border-[#ffffff1c] shadow-xl transition duration-500"
-            : "w-full border-b dark:border-[#ffffff1c] h-[80px] z-[80] dark:shadow"
-        }`}
-      >
-        <div className="w-[95%] 800px:w-[92%] m-auto py-2 h-full">
-          <div className="w-full h-[80px] flex items-center justify-between p-3">
-             <div>
-              <Link href={'/'}
-              className={'text-[25px] font-Poppins font-[500] text-black dark:text-white'}
-                
-              >
-                Elearning
-              </Link>
-             </div>
-          </div>
+    <div
+      className={`${
+        active
+          ? " dark:bg-opacity-50 dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border-[#ffffff1c] shadow-xl transition duration-500"
+          : " w-full  border-b dark:border-[#ffffff1c] h-[80px] z-[80] dark:shadow"
+      }`}
+    >
+      <div className="w-[95%] 800px:w-[92%] m-auto py-2 h-full">
+        <div className="w-full h-[80px] flex items-center justify-between p-3">
+           <div>
+            <Link href={'/'}
+            className={`text-[25px] font-Poppins font-[500] text-black dark:text-white`}
+              
+            >
+              ELearning
+            </Link>
+           </div>
         </div>
       </div>
     </div>
+  </div>
   );
 };
 
