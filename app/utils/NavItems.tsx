7616ad948cbebdee82d420d1751f761b5f28e443
Viplav jha <viplavjha1993@gmail.com>
2023-10-04 19:50:52 +0530

    Navbar  logics

--- app/utils/NavItems.tsx
+++ app/utils/NavItems.tsx
@@ -32,28 +32,40 @@ type Props = {
 const NavItems: React.FC<Props> = ({ activeItem, isMobile }) => {
   return (
     <>
-      <div className="hidden 800px:flex">
-        {navItemsData &&
-          navItemsData.map((i, index) => (
-            <Link href={`${i.url}`} key={index} passHref>
-              <span
-                className={`${
-                  activeItem === index
-                    ? "dark:text-[#37a39a] text-[crimson]"
-                    : "dark:text-white text-black"
-                } 
-                    
-                    text-[18px] px-6 font-Poppins font-[400]`}
-              >
-                {i.name}
-              </span>
-            </Link>
-          ))}
-      </div>
+     <div className="hidden 800px:flex">
+  {navItemsData &&
+    navItemsData.map((i, index) => {
+      return (
+        <Link href={`${i.url}`} key={index} passHref>
+          <span
+            className={`${
+              activeItem === index
+                ? "dark:text-[#37a39a] text-[crimson]"
+                : "dark:text-white text-black"
+            } 
+                
+                text-[18px] px-6 font-Poppins font-[400]`}
+          >
+            {i.name}
+          </span>
+        </Link>
+      );
+    })}
+</div>
+
       {isMobile && (
-        <div className="hidden 800px:flex">
-          {navItemsData &&
-            navItemsData.map((i, index) => (
+        <div className="800px:hidden mt-5">
+        <div className="w-full text-center py-6">
+          <Link href={'/'} passHref >
+            <span
+              className={`text-[25px] font-Poppins font-[500] text-black dark:text-white`}
+            >ELearning</span>
+
+          </Link>
+        </div>
+        {navItemsData &&
+          navItemsData.map((i, index) => {
+            return (
               <Link href={`${i.url}`} key={index} passHref>
                 <span
                   className={`${
@@ -61,14 +73,17 @@ const NavItems: React.FC<Props> = ({ activeItem, isMobile }) => {
                       ? "dark:text-[#37a39a] text-[crimson]"
                       : "dark:text-white text-black"
                   } 
-                    
-                    text-[18px] px-6 font-Poppins font-[400]`}
+                      
+                     block py-5 text-[18px] px-6 font-Poppins font-[400]`}
                 >
                   {i.name}
+                  
                 </span>
               </Link>
-            ))}
-        </div>
+            );
+          })}
+      </div>
+      
       )}
     </>
   );
