07b162c4aea0c8ac9e23325a101b28578096f608
Viplav jha <viplavjha1993@gmail.com>
2023-10-08 16:37:46 +0530

    Login pop-up logics

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ interface Props {}
 const Page: FC<Props> = (props) => {
   const [open, setOpen] = useState(false);
   const [activeItem, setActiveItem] = useState(0);
+  const [route,setRoute] =useState("Login");
 
   return (
     <div>
@@ -20,7 +21,10 @@ const Page: FC<Props> = (props) => {
       />
       <Header open={open} 
        setOpen={setOpen} 
-       activeItem={activeItem} />
+       activeItem={activeItem} 
+       setRoute={setRoute}
+       route={route}
+       />
        <Hero/>
     </div>
   );
