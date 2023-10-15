2d7ac506c71ed9a8c8df78c3ca72901fa7635d65
Viplav jha <viplavjha1993@gmail.com>
2023-10-15 11:11:46 +0530

    api-redux

--- redux/feature/api/apiSlice.ts
+++
@@ -1,9 +0,0 @@
-import {createApi,fetchBaseQuery} from "@reduxjs/toolkit/query/react";
-
-export const apiSlice =createApi({
-    reducerPath:"api",
-    baseQuery:fetchBaseQuery({
-        baseUrl:process.env.NEXT_PUBLIC_SERVER_URI
-    }),
-    endpoints:(builder) =>({})
-})
